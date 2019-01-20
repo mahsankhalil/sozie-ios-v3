c29d8a61d901174957bba1414b24f3a0d5df3793
Omair Baskanderi <omair@sozie.com>
2019-01-20 02:08:34 -0500

    Refactor SelectWorkVC
    Make SelectWorkVC use TitleAndCheckmarkCell. This helped to eliminate
    duplicate code around TableViewCells.

--- Sozie/Controllers/SignUp/SelectWorkVC.swift
+++ Sozie/Controllers/SignUp/SelectWorkVC.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 import SVProgressHUD
+
+struct CellViewModel: RowViewModel, TitleViewModeling, CheckmarkViewModeling {
+    var title: String?
+    var attributedTitle: NSAttributedString?
+    var isCheckmarkEnabled: Bool
+}
+
 class SelectWorkVC: UIViewController {
 
-    @IBOutlet weak var tblVu: UITableView!
+    private let reuseIdentifier = "TitleAndCheckmarkCell"
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTxtFld: UITextField!
     @IBOutlet weak var backBtn: UIButton!
     @IBOutlet weak var nextBtn: DZGradientButton!
     @IBOutlet weak var separatorVu: UIView!
-    var selectedBrandId : Int?
-    var brandList : [Brand]?
-    var searchList : [Brand] = []
-    var isNotFound = false
-    var signUpDict : [String: Any]?
+
+    private var selectedBrandId: Int?
+    private var selectedViewModelIndex: Int?
+    private var brandList: [Brand]?
+    private var signUpDict: [String: Any]?
+    
+    private var searchList: [Brand] = [] {
+        didSet {
+            viewModels.removeAll()
+            if searchList.isEmpty {
+                viewModels = [noSearchResultViewModel]
+            } else {
+                for brand in searchList {
+                    let viewModel = CellViewModel(title: brand.label, attributedTitle: nil, isCheckmarkEnabled: false)
+                    viewModels.append(viewModel)
+                }
+            }
+        }
+    }
+    
+    private lazy var noSearchResultViewModel: CellViewModel = {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor(hex: "#F2A19C")
+        ]
+        let attributedTitle = NSAttributedString(string: "Search not found", attributes: attributes)
+        return CellViewModel(title: nil, attributedTitle: attributedTitle, isCheckmarkEnabled: false)
+    }()
+    
+    private var viewModels: [CellViewModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,59 +60,35 @@ class SelectWorkVC: UIViewController {
         // Do any additional setup after loading the view.
         separatorVu.applyStandardContainerViewShadow()
         searchTxtFld.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        
         fetchDataFromServer()
     }
     
-    
-    func fetchDataFromServer()
-    {
+    func fetchDataFromServer() {
         SVProgressHUD.show()
         ServerManager.sharedInstance.getBrandList(params: [:]) { (isSuccess, response) in
             SVProgressHUD.dismiss()
-            if isSuccess
-            {
+            if isSuccess {
                 self.brandList = response as? [Brand]
                 self.searchList = self.brandList ?? []
-                self.tblVu.reloadData()
+                self.tableView.reloadData()
             }
         }
     }
     
-    @objc func textFieldDidChange(textField : UITextField)
-    {
+    @objc func textFieldDidChange(textField : UITextField) {
         
         searchList.removeAll()
-        if textField.text == ""
-        {
+        guard let brands = brandList, let searchText = textField.text else { return }
+        if textField.text?.isEmpty ?? true {
             searchList = brandList ?? []
-            self.tblVu.reloadData()
+            self.tableView.reloadData()
             return
         }
-        for brand in brandList ?? []
-        {
-            if ((brand.label.lowercased().range(of: textField.text!.lowercased())) != nil)
-            {
-                searchList.append(brand)
-            }
-        }
-        if ((brandList?.count ?? 0) > 0) && (searchList.count == 0)
-        {
-            isNotFound = true
-        }
-        else
-        {
-            isNotFound = false
-        }
+
+        searchList = brands.filter { $0.label.range(of: searchText, options: .caseInsensitive) != nil}
         
-        
-        self.tblVu.reloadData()
-        
-        
+        self.tableView.reloadData()
     }
     
     // MARK Text Field Delegate
@@ -91,8 +100,7 @@ class SelectWorkVC: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        if segue.identifier == "toSignUpEmailVC"
-        {
+        if segue.identifier == "toSignUpEmailVC" {
             let vc = segue.destination as! SignUpEmailVC
             vc.signUpDict = signUpDict
         }
@@ -102,95 +110,66 @@ class SelectWorkVC: UIViewController {
     // MARK: - Actions
     @IBAction func nextBtnTapped(_ sender: Any) {
         
-        if let brandId = selectedBrandId
-        {
+        if let brandId = selectedBrandId {
             signUpDict![User.CodingKeys.brand.stringValue] = brandId
             performSegue(withIdentifier: "toSignUpEmailVC", sender: self)
-        }
-        else
-        {
+        } else {
             UtilityManager.showErrorMessage(body: "Please select Brnad where you work.", in: self)
-
         }
     }
+    
     @IBAction func backBtnTapped(_ sender: Any) {
         self.dismiss(animated: true) {
             
         }
     }
-    
 }
 
 extension SelectWorkVC : UITableViewDelegate , UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isNotFound
-        {
-            return 1
-        }
-        else
-        {
-            return searchList.count
-
-        }
+        return viewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell:WorkCell! = tableView.dequeueReusableCell(withIdentifier: "WorkCell") as? WorkCell
+        var tableViewcell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
         
-        
-        
-        if cell == nil {
-            tableView.register(UINib(nibName: "WorkCell", bundle: nil), forCellReuseIdentifier: "WorkCell")
-            cell = tableView.dequeueReusableCell(withIdentifier: "WorkCell") as? WorkCell
+        if tableViewcell == nil {
+            tableView.register(UINib(nibName: reuseIdentifier, bundle: nil), forCellReuseIdentifier: reuseIdentifier)
+            tableViewcell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
         }
         
-        var notFoundCell:SearchNotFoundCell! = tableView.dequeueReusableCell(withIdentifier: "SearchNotFoundCell") as? SearchNotFoundCell
+        guard let cell = tableViewcell else { return UITableViewCell() }
         
+        cell.selectionStyle = .none
         
-        
-        if notFoundCell == nil {
-            tableView.register(UINib(nibName: "SearchNotFoundCell", bundle: nil), forCellReuseIdentifier: "SearchNotFoundCell")
-            notFoundCell = tableView.dequeueReusableCell(withIdentifier: "SearchNotFoundCell") as? SearchNotFoundCell
+        let viewModel = viewModels[indexPath.row]
+        if let cellConfigurable = cell as? CellConfigurable {
+            cellConfigurable.setup(viewModel)
         }
         
-        if isNotFound
-        {
-            return notFoundCell
-        }
-        else
-        {
-            let brand = searchList[indexPath.row]
-            cell.titleLbl.text = brand.label
-            if selectedBrandId == brand.brandId
-            {
-                cell.checkMarkImgVu.isHidden = false
-            }
-            else
-            {
-                cell.checkMarkImgVu.isHidden = true
-            }
-            
-            
-            return cell
-        }
-        
-        
-        
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        if !isNotFound
-        {
-            tableView.deselectRow(at: indexPath, animated: true)
-            let currentBrand = searchList[indexPath.row]
-            selectedBrandId = currentBrand.brandId
-            tblVu.reloadData()
-        }
-        
+        return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if !searchList.isEmpty {
+            
+            selectedBrandId = searchList[indexPath.row].brandId
+            
+            var indexPathsToReload = [indexPath]
+            if let previousSelectedIndex = selectedViewModelIndex {
+                viewModels[previousSelectedIndex].isCheckmarkEnabled = false
+                indexPathsToReload.append(IndexPath(row: previousSelectedIndex, section: 0))
+            }
+            
+            viewModels[indexPath.row].isCheckmarkEnabled = true
+            selectedViewModelIndex = indexPath.row
+
+            tableView.reloadRows(at: indexPathsToReload, with: .automatic)
+        }
+    }
 }
+
 extension SelectWorkVC: SignUpInfoProvider {
     var signUpInfo: [String: Any]? {
         get { return signUpDict }
