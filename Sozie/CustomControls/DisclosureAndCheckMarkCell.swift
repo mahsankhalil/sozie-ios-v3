0995eb13e2e9306595263273f7f386516468b0fb
zaighumghazali <zaighumghazali>
2020-05-21 15:21:39 +0500

    DEV-2002 and tutorial changes

---
+++ Sozie/CustomControls/DisclosureAndCheckMarkCell.swift
@@ -0,0 +1,24 @@
+//
+//  DisclosureAndCheckMarkCell.swift
+//  Sozie
+//
+//  Created by Zaighum Ghazali Khan on 5/21/20.
+//  Copyright © 2020 Danial Zahid. All rights reserved.
+//
+
+import UIKit
+
+class DisclosureAndCheckMarkCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
